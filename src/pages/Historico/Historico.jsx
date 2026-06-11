e41762f5c99c70f60fa2e9c033f2dc43781d3193
roniealmeida <ronialmeida.contato@gmail.com>
2026-06-11 19:06:34 -0300

    historico finalizado

--- src/pages/Historico/Historico.jsx
+++ src/pages/Historico/Historico.jsx
@@ -1,26 +1,25 @@
 import { useState } from "react";
-// Removido o "Link" daqui se não for usar no texto, mas mantido o resto corrigido:
-import MenuLateral from "../MenuLateral/MenuLateral"; // importa o menu
+import { Link } from "react-router-dom"; // Importado para fazer o redirecionamento
+import MenuLateral from "../MenuLateral/MenuLateral"; 
 import iconMenu from "../../assets/images/icon-menu.png";
 import logo from "../../assets/images/Logo-AnalisaAI.png";
 import iconPerson from "../../assets/images/icon-person.png";
 
-import "../Home/Home.css"; // Importação correta do CSS do Histórico
+import "../Home/Home.css"; 
 import "./Historico.css";
-// Se o CSS do menu estiver em outra pasta, importe direto assim: import "../MenuLateral/MenuLateral.css";
 
 export default function Historico() {
-  const [menuAberto, setMenuAberto] = useState(false); //menu
-  
+  const [menuAberto, setMenuAberto] = useState(false); 
+
   return (
     <>
       <header>
-        <div id="cabecalho"> 
+        <div id="cabecalho">
           <img
             id="icon-menu"
             src={iconMenu}
             alt="Menu"
-            onClick={() => setMenuAberto(true)} // Abre o menu
+            onClick={() => setMenuAberto(true)} 
           />
 
           <img id="logo" src={logo} alt="Logo" />
@@ -28,20 +27,121 @@ export default function Historico() {
           <img id="icon-person" src={iconPerson} alt="Perfil" />
         </div>
       </header>
-
       <main>
-        <div id="title">
-        <h2>Histórico</h2>
+        <div id="title-historic">
+          <h2>Histórico</h2>
+          <p>Confira seu historico de pesquisa</p>
         </div>
 
         <div id="listagem">
+          <Link to="/nova-pagina" style={{ textDecoration: 'none', color: 'inherit' }}>
           <div id="box-plant">
-            //Colocar a imagem e as informações da planta
+            <div id="image-plant">
+              <img src="src/assets/images/fototeste.jpeg" alt="Planta" />
+            </div>
+
+            <div id="info-plant">
+              <div id="title-plant">
+                <h3>Nome da planta</h3>
+              </div>
+
+              <div id="description-plant">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae...
+                </p>
+              </div>
+              
+              {/* Seta com ação de redirecionamento */}
+              <div id="button-seta"> 
+                  <img src="src/assets/images/icon-seta.png" alt="Avançar" />
+                
+                <p>
+                  Saiba mais
+                </p>
+              </div>
+              
+            </div>
           </div>
+          </Link>
+        </div>
+
+        <div id="listagem">
+          <Link to="/nova-pagina" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <div id="box-plant">
+            <div id="image-plant">
+              <img src="src/assets/images/fototeste.jpeg" alt="Planta" />
+            </div>
+
+            <div id="info-plant">
+              <div id="title-plant">
+                <h3>Nome da planta</h3>
+              </div>
+
+              <div id="description-plant">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae...
+                </p>
+              </div>
+              
+              {/* Seta com ação de redirecionamento */}
+              <div id="button-seta"> 
+                  <img src="src/assets/images/icon-seta.png" alt="Avançar" />
+                
+                <p>
+                  Saiba mais
+                </p>
+              </div>
+              
+            </div>
+          </div>
+          </Link>
+        </div>
+
+        <div id="listagem">
+          <Link to="/nova-pagina" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <div id="box-plant">
+            <div id="image-plant">
+              <img src="src/assets/images/fototeste.jpeg" alt="Planta" />
+            </div>
+
+            <div id="info-plant">
+              <div id="title-plant">
+                <h3>Nome da planta</h3>
+              </div>
+
+              <div id="description-plant">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Tempora, porro perferendis suscipit quis pariatur eligendi
+                  neque recusandae eius quae...
+                </p>
+              </div>
+              
+              {/* Seta com ação de redirecionamento */}
+              <div id="button-seta"> 
+                  <img src="src/assets/images/icon-seta.png" alt="Avançar" />
+                
+                <p>
+                  Saiba mais
+                </p>
+              </div>
+              
+            </div>
+          </div>
+          </Link>
         </div>
       </main>
-
-      <MenuLateral menuAberto={menuAberto} setMenuAberto={setMenuAberto} /> //coloca isso de menu tambem
+      <MenuLateral menuAberto={menuAberto} setMenuAberto={setMenuAberto} />
     </>
   );
 }

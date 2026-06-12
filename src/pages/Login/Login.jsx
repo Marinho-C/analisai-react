52158c3e9a82240378ba2e4f38f9d2e03a75e0da
Marinho-C <ceciliamarinho1105@gmail.com>
2026-06-12 19:07:18 -0300

    Coloquei o olho magico da senha e centralizei a frase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Login.css";
-
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import logo from "../../assets/images/Logo-AnalisaAI.png";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [ver, setVer] = useState(false);
 
   function entrar() {
     navigate("/home");
@@ -12,52 +14,52 @@ export default function Login() {
 
   return (
     <>
-      
       <div id="login-container">
-      <div id="logo">
-        <img src={logo} alt="Logo AnalisaAI" />
-      </div>
-
-      <div id="title">
-        <h2>Realize seu acesso</h2>
-      </div>
-
-      </div>
-
-      <div id="inputs">
-
-        <div id="telefone">
-          <label>Telefone</label>
-
-          <input
-            id="inputTelefone"
-            type="tel"
-            placeholder="Digite seu telefone"
-          />
+        <div id="logo">
+          <img src={logo} alt="Logo AnalisaAI" />
         </div>
 
-        <div id="senha">
-          <label>Senha</label>
-
-          <input
-            id="inputSenha"
-            type="password"
-            placeholder="Digite sua senha"
-          />
+        <div className="textos">
+          <h2>Realize seu acesso</h2>
         </div>
 
-      </div>
+        <div id="inputs">
+          <div id="telefone">
+            <label>Telefone</label>
+            <input
+              id="inputTelefone"
+              type="tel"
+              placeholder="Digite seu telefone"
+            />
+          </div>
 
-      <div id="buttons">
+          <div id="senha">
+            <label>Senha</label>
+            <div className="caixa-input">
+              <input
+                id="inputSenha"
+                type={ver ? "text" : "password"}
+                placeholder="Digite sua senha"
+              />
+              <button
+                type="button"
+                onClick={() => setVer(!ver)}
+                className="botao-olho"
+              >
+                {ver ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+              </button>
+            </div>
+          </div>
+        </div>
 
-        <button id="entrar" onClick={entrar}>
-          Entrar
-        </button>
-
-        <button id="cadastrar">
-          Cadastrar-se
-        </button>
-
+        <div id="buttons">
+          <button id="entrar" onClick={entrar}>
+            Entrar
+          </button>
+          <button id="cadastrar">
+            Cadastrar-se
+          </button>
+        </div>
       </div>
     </>
   );
